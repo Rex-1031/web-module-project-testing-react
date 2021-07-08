ec8cabc85ed4ab7564acc6816e5109a9208e1eb5
Steve Clair <64785141+Rex-1031@users.noreply.github.com>
2021-07-07 20:03:03 -0400

    episode component completed

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,24 +8,40 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
-    summary: "",
+    summary: "lorem ipsum",
     runtime: 1
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id: 1,
+    name: "Stranger Things",
+    img: null,
+    season: 1,
+    number: 1,
+    summary: "Lorem Ipsum",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={[]}/>)
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>)
+
+    let summary = screen.getByText(`${testEpisode.summary}`)
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent(/lorem/i)
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage} />)
+    const altImg = screen.getByAltText('./stranger_things.png');
+
+    expect(altImg).toBeInTheDocument();
+    expect(altImg).toBeTruthy();
 })
 
 //Tasks
